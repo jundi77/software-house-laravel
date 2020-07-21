7b0a021c650cf1b0546a313cbc796fdc7c5b05bd
Jundullah H. R <jundihanif77@gmail.com>
2020-07-21 10:19:08 +0700

    Menyatukan query mencari dan ambil pertanyaan

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -29,23 +29,20 @@ class QuestionController extends Controller
   
     public function index(Request $request)
     {
+        $questions = DB::table('questions')
+                                ->select('questions.id as question_id',
+                                         'questions.user_id as author_id',
+                                         'question',
+                                         'questions.created_at',
+                                         'questions.updated_at',
+                                         'users.name as author_name',
+                                         'users.profile_picture_path')
+                                ->leftJoin('users','user_id','=','users.id');
+
         if($request->has('search')){
             // cek kalau mau mencari pertanyaan
-            // return dd($request->search);
-            $found = DB::table('questions')
-            ->select('questions.id as question_id',
-                     'questions.user_id as author_id',
-                     'question',
-                     'questions.created_at',
-                     'questions.updated_at',
-                     'users.name as author_name',
-                     'users.profile_picture_path')
-            ->leftJoin('users','user_id','=','users.id')
-            ->where('question', 'like', '%'.$request->search.'%')
-            ->get();
-            // return dd([$request->search,$found]);
-        }else {
-            // user mau menampilkan semua pertanyaan
+            $questions = $questions->where('question', 'like', '%'.$request->search.'%');
         }
+        return dd($questions->get());
     }
 }
