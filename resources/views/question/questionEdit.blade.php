e7ea4b02d91ac1c71456205564e3d58e062539bd
gilbertk27 <gilberthariyanto.190511@mhs.its.ac.id>
2020-07-23 20:52:10 +0700

    Added edit question page

---
+++ resources/views/question/questionEdit.blade.php
@@ -0,0 +1,34 @@
+@extends('include.template')
+
+@section('content')
+    <section class="question-section">
+        <div class="container">
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="question-wrapper">
+                        <h2>Edit Pertanyaan</h2>
+                        <div class="question-list">
+                            @forelse ($questions as $question)
+		                                {{ csrf_field() }}
+                                        <form>
+                                            <div class="form-group" action="{{route('question.update',['id' => $question->id])}}" method="post">
+                                                <label for="InputTitle">Title</label>
+                                                <input type="text" name="Title" class="form-control">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="InputQuestion">Pertanyaan</label>
+                                                <textarea class="form-control" type="text" name="Question" rows="5"></textarea>
+                                            </div>
+                                                <button type="submit" class="btn btn-primary">Edit</button>
+                                        </form>
+                                </div>
+                            @empty
+                            @endforelse
+                        </div>
+                    </div>
+                </div>
+                {{$questions->links()}}
+            </div>
+        </div>
+    </section>
+@endsection
