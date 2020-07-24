e0d7d128d8a4bb0c8698bbb10324f8a4b20b94ad
Jundullah H. R <jundihanif77@gmail.com>
2020-07-24 23:50:30 +0700

    Menghapus footer untuk route('homepage')

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -14,42 +14,4 @@
         </div>
     </div>
 </section>
-<section id="feature">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="text-center">
-                    <h3 class="text-judul text-white">
-                        <span>
-                        Feature
-                        </span>
-                    </h3>
-                </div>
-                <div class="feature-wrapper">
-                    <div class="feature-items">
-                        <div class="feature-icon">
-                            <i class="fa fa fa-search"></i>
-                        </div>
-                        <h4 class="feature-caption text-white">Pencarian Mahasiswa</h4>
-                        <p class="text-center text-white">Edustage dapat mencari data berbagai mahasiswa yang ada</p>
-                    </div>
-                    <div class="feature-items">
-                        <div class="feature-icon">
-                            <i class="fa fa fa-phone"></i>
-                        </div>
-                        <h4 class="feature-caption text-white">Website Responsive</h4>
-                        <p class="text-center text-white">Tampilan Edustage dapat dibuka di segala jenis perangkat</p>
-                    </div>
-                    <div class="feature-items">
-                        <div class="feature-icon">
-                            <i class="fa fa fa-book"></i>
-                        </div>
-                        <h4 class="feature-caption text-white">Informasi Mahasiswa</h4>
-                        <p class="text-center text-white">Edustage dapat menyediakan informasi pribadi mahasiswa</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
 @endsection
